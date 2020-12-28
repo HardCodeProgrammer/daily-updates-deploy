186a231dbf80ac57752d082da40287a6f0e23a0f
HardCodeProgrammer <svenkataramanain@gmail.com>
2020-12-28 16:00:11 +0530

    Updates

---
+++ precache-manifest.3b9cf9b1c60671a4aaac14f3ce3f07d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cf3a5295e20fc58a6da02cdbd947e8",
+    "url": "/daily-updates-deploy/index.html"
+  },
+  {
+    "revision": "d7bb33e751a3e6a58e03",
+    "url": "/daily-updates-deploy/static/css/main.bd11927d.chunk.css"
+  },
+  {
+    "revision": "d7033952fca66b518b50",
+    "url": "/daily-updates-deploy/static/js/2.75293809.chunk.js"
+  },
+  {
+    "revision": "371b09233cb37944352df475dc41cc4d",
+    "url": "/daily-updates-deploy/static/js/2.75293809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7bb33e751a3e6a58e03",
+    "url": "/daily-updates-deploy/static/js/main.0912a64b.chunk.js"
+  },
+  {
+    "revision": "19a7c76f0df81e06bb35",
+    "url": "/daily-updates-deploy/static/js/runtime-main.84fb8f70.js"
+  }
+]);
